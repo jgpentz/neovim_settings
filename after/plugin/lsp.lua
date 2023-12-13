a6f21f671fa8d25dce0941bf8b1357baa9747283
J. Pentz <jpentz1@gmail.com>
2023-12-12 21:59:21 -0700

    adding autocomplete cmp mappings.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -51,3 +51,23 @@ lspconfig.lua_ls.setup {
     },
   },
 }
+
+local cmp = require('cmp')
+
+cmp.setup({
+   mapping = {
+      ["<C-p>"] = cmp.mapping.select_prev_item(),
+      ["<C-n>"] = cmp.mapping.select_next_item(),
+      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.close(),
+      ["<CR>"] = cmp.mapping.confirm({
+         behavior = cmp.ConfirmBehavior.Replace,
+         select = true,
+      }),
+      ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
+      ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
+
+    }
+})
