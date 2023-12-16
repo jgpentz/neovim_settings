c1f8f1aad0cd9e0d8fa3c4c1ccf0ba189a3e96ef
J. Pentz <jpentz1@gmail.com>
2023-12-15 18:28:55 -0700

    Added support for the hack hdl

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -19,3 +19,24 @@ require'nvim-treesitter.configs'.setup {
     additional_vim_regex_highlighting = false,
   },
 }
+
+
+-- Configuration for the hack hdl (nand2tetris) parser
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.hackhdl= {
+  install_info = {
+    url = "~/Code/tree-sitter-nand2tetris/tree-sitter-hackhdl", -- local path or git repo
+    files = {"src/parser.c"}, -- note that some parsers also require src/scanner.c or src/scanner.cc
+    -- optional entries:
+    branch = "main", -- default branch in case of git repo if different from master
+    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+    requires_generate_from_grammar = true, -- if folder contains pre-generated src/parser.c
+  },
+  filetype = "hdl", -- if filetype does not match the parser name
+}
+
+vim.filetype.add({
+    extension = {
+        hdl = 'hackhdl',
+    }
+})
