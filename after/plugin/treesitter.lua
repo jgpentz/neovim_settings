5123a3b5bc2fe563946bf49e34517bda2923e924
J. Pentz <jpentz1@gmail.com>
2024-01-11 13:29:21 -0700

    Added neorg

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -40,3 +40,22 @@ vim.filetype.add({
         hdl = 'hackhdl',
     }
 })
+
+-- Configuration for the hack asm (nand2tetris) parser
+parser_config.hackasm= {
+  install_info = {
+    url = "~/Code/tree-sitter-nand2tetris/tree-sitter-hackasm", -- local path or git repo
+    files = {"src/parser.c"}, -- note that some parsers also require src/scanner.c or src/scanner.cc
+    -- optional entries:
+    branch = "main", -- default branch in case of git repo if different from master
+    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+    requires_generate_from_grammar = true, -- if folder contains pre-generated src/parser.c
+  },
+  filetype = "asm", -- if filetype does not match the parser name
+}
+
+vim.filetype.add({
+    extension = {
+        hdl = 'hackasm',
+    }
+})
---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,4 @@
+return {
+    'nvim-neorg/neorg',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+}
